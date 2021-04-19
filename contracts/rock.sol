2e4313405485b75f9a27f2d4d49e4e73c4e951b5
goldencat-tom <bluebird6922@gmail.com>
2021-04-19 11:23:45 +0300

    fix transfer ownership

--- contracts/rock.sol
+++ contracts/rock.sol
@@ -49,6 +49,7 @@ contract Ownable is Context {
     }
 
     function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0), 'Ownable: new owner is the zero address');
         emit OwnershipTransferred(_owner, newOwner);
         _owner = newOwner;
     }
