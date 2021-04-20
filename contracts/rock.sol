c7a95faf8d9c15430ecba465937561c68ef7a657
goldencat-tom <bluebird6922@gmail.com>
2021-04-20 21:27:32 +0300

    mod methods

--- contracts/rock.sol
+++ contracts/rock.sol
@@ -149,6 +149,22 @@ library SafeMath {
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         return div(a, b, 'SafeMath: division by zero');
     }
+    
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        return c;
+    }
+    
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, 'SafeMath: modulo by zero');
+    }
+
+     */
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        return a % b;
+    }
+
 }
 
 library Address {
