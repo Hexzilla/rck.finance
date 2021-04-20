57b45b43bd6ac1b9259d01e046f796956397febe
goldencat-tom <bluebird6922@gmail.com>
2021-04-20 21:28:17 +0300

    min error fix

--- contracts/rock.sol
+++ contracts/rock.sol
@@ -140,6 +140,10 @@ library SafeMath {
     }
     
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
         uint256 c = a * b;
         require(c / a == b, 'SafeMath: multiplication overflow');
 
@@ -153,6 +157,8 @@ library SafeMath {
     function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
         require(b > 0, errorMessage);
         uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
         return c;
     }
     
