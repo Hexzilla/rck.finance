3004a4fdeb11cec8449c7d9854d040ed4b9459e8
goldencat-tom <bluebird6922@gmail.com>
2021-04-20 21:26:08 +0300

    add, sub methods

--- contracts/rock.sol
+++ contracts/rock.sol
@@ -122,7 +122,22 @@ interface IBEP20 {
 }
 
 library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
 
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, 'SafeMath: subtraction overflow');
+    }
+
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
 }
 
 library Address {
