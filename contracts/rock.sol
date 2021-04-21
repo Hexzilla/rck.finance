b9c56cbfdf38817c208533a523d6fdf902a392a1
goldencat-tom <bluebird6922@gmail.com>
2021-04-21 14:20:26 +0300

    ignore state in reentrant

--- contracts/rock.sol
+++ contracts/rock.sol
@@ -193,15 +193,15 @@ abstract contract ReentrancyGuard {
 
     /**
      * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and make it call a
+     * `private` function that does the actual work.
      */
     modifier nonReentrant() {
         // On the first call to nonReentrant, _notEntered will be true
         require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
 
-        // Any calls to nonReentrant after this point will fail
-        _status = _ENTERED;
-
-        _;
 
         // By storing the original value once again, a refund is triggered (see
         // https://eips.ethereum.org/EIPS/eip-2200)
