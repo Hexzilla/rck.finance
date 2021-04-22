14cf92d28b56ad5912e7fffadf50aab75f540c72
goldencat-tom <bluebird6922@gmail.com>
2021-04-22 18:32:17 +0300

    functionCall 2

--- contracts/rock.sol
+++ contracts/rock.sol
@@ -223,6 +223,14 @@ library Address {
         return _functionCallWithValue(target, data, 0, errorMessage);
     }
 
+    function functionCall(
+        address target,
+        bytes memory data,
+        string memory errorMessage
+    ) internal returns (bytes memory) {
+        return _functionCallWithValue(target, data, 0, errorMessage);
+    }
+
 
 }
 
