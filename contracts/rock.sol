c55de870816170aebf0d56bffcd8eca8ea9c7c60
goldencat-tom <bluebird6922@gmail.com>
2021-04-22 18:32:07 +0300

    functionCall 2

--- contracts/rock.sol
+++ contracts/rock.sol
@@ -214,6 +214,15 @@ library Address {
     function functionCall(address target, bytes memory data) internal returns (bytes memory) {
         return functionCall(target, data, 'Address: low-level call failed');
     }
+    
+    function functionCall(
+        address target,
+        bytes memory data,
+        string memory errorMessage
+    ) internal returns (bytes memory) {
+        return _functionCallWithValue(target, data, 0, errorMessage);
+    }
+
 
 }
 
