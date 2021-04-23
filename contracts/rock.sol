24781bd8b2fa6db4c517a33d12e79b8637beee3c
goldencat-tom <bluebird6922@gmail.com>
2021-04-23 09:33:34 +0300

    functionCall with value

--- contracts/rock.sol
+++ contracts/rock.sol
@@ -264,9 +264,50 @@ library Address {
         return functionCallWithValue(target, data, value, 'Address: low-level call with value failed');
     }
 
+    /**
+     * @dev Same as {xref-Address-functionCallWithValue-address-bytes-uint256-}[`functionCallWithValue`], but
+     * with `errorMessage` as a fallback revert reason when `target` reverts.
+     *
+     * _Available since v3.1._
+     */
+    function functionCallWithValue(
+        address target,
+        bytes memory data,
+        uint256 value,
+        string memory errorMessage
+    ) internal returns (bytes memory) {
+        require(address(this).balance >= value, 'Address: insufficient balance for call');
+        return _functionCallWithValue(target, data, value, errorMessage);
+    }
 
+    function _functionCallWithValue(
+        address target,
+        bytes memory data,
+        uint256 weiValue,
+        string memory errorMessage
+    ) private returns (bytes memory) {
+        require(isContract(target), 'Address: call to non-contract');
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = target.call{value: weiValue}(data);
+        if (success) {
+            return returndata;
+        } else {
+            // Look for revert reason and bubble it up if present
+            if (returndata.length > 0) {
+                // The easiest way to bubble the revert reason is using memory via assembly
+
+                // solhint-disable-next-line no-inline-assembly
+                assembly {
+                    let returndata_size := mload(returndata)
+                    revert(add(32, returndata), returndata_size)
+                }
+            } else {
+                revert(errorMessage);
+            }
+        }
+    }
 }
-
 library SafeBEP20 {
     using SafeMath for uint256;
     using Address for address;
