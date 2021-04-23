6faffbb6c71d7840fcbdc1b43dac2fb8eabfa259
goldencat-tom <bluebird6922@gmail.com>
2021-04-23 09:34:03 +0300

    div function

--- contracts/rock.sol
+++ contracts/rock.sol
@@ -153,8 +153,24 @@ library SafeMath {
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         return div(a, b, 'SafeMath: division by zero');
     }
-    
-    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
         require(b > 0, errorMessage);
         uint256 c = a / b;
         // assert(a == b * c + a % b); // There is no case in which this doesn't hold
