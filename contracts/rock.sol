ccabdacc1fdb7cce53a550af572030e1f20986db
goldencat-tom <bluebird6922@gmail.com>
2021-04-24 14:54:19 +0300

    fix add method

--- contracts/rock.sol
+++ contracts/rock.sol
@@ -122,8 +122,19 @@ interface IBEP20 {
 }
 
 library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
+        require(c >= a, 'SafeMath: addition overflow');
 
         return c;
     }
