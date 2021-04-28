ab70486ba23a55fec00f2b2f9140125128567e25
goldencat-tom <bluebird6922@gmail.com>
2021-04-28 10:40:02 +0300

    update IBEP20

--- contracts/rock.sol
+++ contracts/rock.sol
@@ -79,12 +79,22 @@ interface IBEP20 {
      */
     function name() external view returns (string memory);
 
+    /**
+     * @dev Returns the bep token owner.
+     */
     function getOwner() external view returns (address);
 
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
     function balanceOf(address account) external view returns (uint256);
 
     /**
      * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
      */
     function transfer(address recipient, uint256 amount) external returns (bool);
 
