3111df8a4cd38daf41ad0ac2667baafff5ac57b7
goldencat-tom <bluebird6922@gmail.com>
2021-04-29 10:40:18 +0300

    ownable context

--- contracts/rock.sol
+++ contracts/rock.sol
@@ -38,17 +38,29 @@ contract Ownable is Context {
         _;
     }
 
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
     function renounceOwnership() public onlyOwner {
         emit OwnershipTransferred(_owner, address(0));
         _owner = address(0);
     }
 
-     * @dev Transfers ownership of the contract.
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
      */
     function transferOwnership(address newOwner) public onlyOwner {
         _transferOwnership(newOwner);
     }
 
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     */
     function _transferOwnership(address newOwner) internal {
         require(newOwner != address(0), 'Ownable: new owner is the zero address');
         emit OwnershipTransferred(_owner, newOwner);
