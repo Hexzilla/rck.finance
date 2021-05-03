70314ac8f8286f5a9139edf6113e474880012df8
goldencat-tom <bluebird6922@gmail.com>
2021-05-03 18:23:40 +0300

    submit

--- contracts/rock.sol
+++ contracts/rock.sol
@@ -1,16 +1,22 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-05-03
+*/
+
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.12;
 
+
 contract Context {
-    // Empty internal constructor
+    // Empty internal constructor, to prevent people from mistakenly deploying
+    // an instance of this contract, which should be used via inheritance.
     constructor() internal {}
 
     function _msgSender() internal view returns (address payable) {
-      return msg.sender;
+        return msg.sender;
     }
 
     function _msgData() internal view returns (bytes memory) {
-        this; // silence state mutability warning without generating bytecode
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
         return msg.data;
     }
 }
